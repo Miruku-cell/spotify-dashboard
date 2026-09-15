e67141c3558c77769b89efeb8af7f74570c3ddbf
Miruku-cell <zanyesitt323@gmail.com>
2026-09-15 23:36:54 +0630

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
 import seaborn as sns
+import glob
 
 # Scikit-learn Tools
 # Scikit-learn Tools
